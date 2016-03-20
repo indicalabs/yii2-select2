a8cfeb57a758ec9ef2b5d5046c67c22506698357
indicalabs <venu.narukulla@gmail.com>
2016-03-20 11:17:55 +0000

    Adding files to git

---
+++ Select2BootstrapAsset.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link https://github.com/indicalabs/yii2-select2
+ * @copyright Copyright (c) 2016 Venu Narukulla
+ * @license https://github.com/indicalabs/yii2-select2/blob/master/LICENSE
+ */
+
+namespace indicalabs\select2;
+
+/**
+ * @author 
+ * @link http://select2.github.io/select2-bootstrap-theme/
+ */
+class Select2BootstrapAsset extends \yii\web\AssetBundle
+{
+    // The files are not web directory accessible, therefore we need
+    // to specify the sourcePath property. Notice the @bower alias used.
+    public $sourcePath = '@bower/select2-bootstrap-theme/dist';
+    
+    public $css = [
+        'select2-bootstrap.min.css',
+    ];
+    
+    public $depends = [
+        'yii\bootstrap\BootstrapAsset',
+        'indicalabs\select2\Select2Asset',
+    ];
+}
--- Select2Widget.php
+++ Select2Widget.php
@@ -3,7 +3,8 @@
 namespace indicalabs\select2;
 
 use yii\helpers\Html;
-use yii\jui\InputWidget;
+use yii\helpers\Url;
+use conquer\helpers\Json;
 
 /**
  * Wrapper Widget to use jQuery Select2 in Yii2 application.
@@ -77,50 +78,150 @@ use yii\jui\InputWidget;
  * } 
  * @see https://github.com/ivaynberg/select2 jQuery Select2
  */
-class Select2Widget extends InputWidget
+class Select2Widget extends \yii\widgets\InputWidget
 {
 
-	public $language = false;
-	
-	/** @var bool Multiple or single item should be selected */
-	public $multiple = false;
 
-	/** @var array Array of items to list in the dropdown */
+	/**
+	 * Points to use Bootstrap theme
+	 * @var boolean
+	 */
+	public $bootstrap = true;
+	/**
+	 * Language code
+	 * Set False to disable
+	 * @var string | boolean
+	 */
+	public $language;
+	/**
+	 * Array data
+	 * @example [['id'=>1, 'text'=>'enhancement'], ['id'=>2, 'text'=>'bug']]
+	 * @var array
+	 */
 	public $data;
+	/**
+	 * You can use Select2Action to provide AJAX data
+	 * @see \yii\helpers\BaseUrl::to()
+	 * @var array|string
+	 */
+	public $ajax;
+	/**
+	 * @see \yii\helpers\BaseArrayHelper::map()
+	 * @var array
+	 */
+	public $items = [];
+	/**
+	 * A placeholder value can be defined and will be displayed until a selection is made
+	 * @var string
+	 */
+	public $placeholder;
+	/**
+	 * Multiple select boxes
+	 * @var boolean
+	 */
+	public $multiple;
+	/**
+	 * Tagging support
+	 * @var boolean
+	 */
+	public $tags;
+	/**
+	 * @link https://select2.github.io/options.html
+	 * @var array
+	 */
+	public $settings = [];
 	
+	/**
+	 * @inheritdoc
+	 */
 	public function init()
 	{
 		parent::init();
-		
-		$this->options['multiple'] = $this->multiple ? true : false;
+	
+		if ($this->tags) {
+			$this->options['data-tags'] = 'true';
+			$this->options['multiple'] = true;
+		}
+		if ($this->language) {
+			$this->options['data-language'] = $this->language;
+		}
+		if (!is_null($this->ajax)) {
+			$this->options['data-ajax--url'] = Url::to($this->ajax);
+			$this->options['data-ajax--cache'] = 'true';
+		}
+		if ($this->placeholder) {
+			$this->options['data-placeholder'] = $this->placeholder;
+		}
+		if ($this->multiple) {
+			$this->options['data-multiple'] = 'true';
+			$this->options['multiple'] = true;
+		}
+		if (!empty($this->data)) {
+			$this->options['data-data'] = \yii\helpers\Json::encode($this->data);
+		}
+		if (!isset($this->options['class'])) {
+			$this->options['class'] = 'form-control';
+		}
+		if ($this->bootstrap) {
+			$this->options['data-theme'] = 'bootstrap';
+		}
+		if ($this->multiple || !empty($this->settings['multiple'])) {
+			if ($this->hasModel()) {
+				$name = isset($this->options['name']) ? $this->options['name'] : Html::getInputName($this->model, $this->attribute);
+			} else {
+				$name = $this->name;
+			}
+			if (substr($name, -2) != '[]') {
+				$this->options['name'] = $this->name = $name . '[]';
+			}
+		}
 	}
-
-	/** Render widget html and register client scripts */
+	
+	/**
+	 * @inheritdoc
+	 */
 	public function run()
 	{
-		if ($this->language !== false) {
-			$view = $this->getView();
-			Select2RegionalAsset::register($view);
-			
-			/** Register language asstets in the above Select2RegionalAsset and delete the below
-				$lang = strtoupper(str_replace('_', '-', Yii::$app->language));
-				$lang[0] = strtolower($lang[0]);
-				$lang[1] = strtolower($lang[1]);
-
-				$cs->registerScriptFile($this->assetsDir . $src . '/select2_locale_' . $lang . $min . '.js');
-			 */
-
-			$this->registerWidget('select2', Select2Asset::className());
+		if ($this->hasModel()) {
+			echo Html::activeDropDownList($this->model, $this->attribute, $this->items, $this->options);
 		} else {
-			$this->registerWidget('select2', Select2Asset::className());
+			echo Html::dropDownList($this->name, $this->value, $this->items, $this->options);
 		}
-		echo $this->renderWidget() . "\n";
+		$this->registerAssets();
+	}
+	
+	/**
+	 * Registers Assets
+	 */
+	public function registerAssets()
+	{
+		$view = $this->getView();
+		$bandle = Select2Asset::register($view);
+		if ($this->language !== false) {
+			$langs[0] = $this->language ? $this->language : \Yii::$app->language;
+			if (($pos = strpos($langs[0], '-')) > 0) {
+				// If "en-us" is not found, try to use "en".
+				$langs[1] = substr($langs[0], 0, $pos);
+			}
+			foreach ($langs as $lang) {
+				$langFile = "/js/i18n/{$lang}.js";
+				if (file_exists($bandle->sourcePath . $langFile)) {
+					$view->registerJsFile($bandle->baseUrl . $langFile, ['depends' => Select2Asset::className()]);
+					break;
+				}
+			}
+		}
+		if ($this->bootstrap) {
+			Select2BootstrapAsset::register($view);
+		}
+		$settings = Json::encode($this->settings);
+		$view->registerJs("jQuery('#{$this->options['id']}').select2($settings);");
 	}
 
 	/**
 	 * Renders the AutoComplete widget.
 	 * @return string the rendering result.
-	 */
+	 
 	public function renderWidget()
 	{
 		$contents = [];
@@ -139,6 +240,6 @@ class Select2Widget extends InputWidget
 				}
 		return implode("\n", $contents);
 	}
-
+*/
 }
 
